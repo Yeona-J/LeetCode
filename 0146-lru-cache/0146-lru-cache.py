3d7c95fa1fb4650374b9a66edf9a50cef6403da2
Yeona Jeong <yeon.aa.j@gmail.com>
2023-04-26 17:24:37 +0900

    Time: 2715 ms (6.34%), Space: 77.5 MB (7.54%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.c = capacity
+        self.m = dict()
+        self.deq = deque()
+
+    def get(self, key: int) -> int:
+        if key in self.m:
+            value = self.m[key]
+            self.deq.remove(key)
+            self.deq.append(key)
+            return value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.m:
+            if len(self.deq) == self.c:
+                oldest = self.deq.popleft()
+                del self.m[oldest]
+        else:
+            self.deq.remove(key)
+        
+        self.m[key] = value
+        self.deq.append(key)
+        
